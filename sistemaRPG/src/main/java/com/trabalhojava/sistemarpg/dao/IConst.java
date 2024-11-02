340ca49187d17d96530da3c9fd7f267bafd76d14
carboniart <arthurlinzing@yahoo.com>
2024-11-02 13:16:21 -0300

    atualizacao do menu, gif + rgb.

--- sistemaRPG/src/main/java/com/trabalhojava/sistemarpg/dao/IConst.java
+++
@@ -1,7 +0,0 @@
-package com.trabalhojava.sistemarpg.dao;
-
-public interface IConst {
-    String stringDeConexao = "jdbc:postgresql://localhost:5432/sistemarpg";
-    String Usuario = "postgres";
-    String Senha = "postgres";
-}
